753c593d21dd9d64e3c595df67db3531559a25dd
feng <123@qq.com>
2021-01-26 23:34:55 +0800

    to elementui admin

--- leu-element-ui/src/App.vue
+++ leu-element-ui/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <router-view />
   </div>
 </template>
 
@@ -9,12 +9,3 @@ export default {
   name: 'App'
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-</style>
--- leu-element-ui/src/main.js
+++ leu-element-ui/src/main.js
@@ -1,24 +1,53 @@
 import Vue from 'vue'
-import App from './App.vue'
+
+import Cookies from 'js-cookie'
+
+import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+
+import Element from 'element-ui'
+import './styles/element-variables.scss'
+import enLang from 'element-ui/lib/locale/lang/en'// 如果使用中文语言包请默认支持，无需额外引入，请删除该依赖
+
+import '@/styles/index.scss' // global css
+
+import App from './App'
+import store from './store'
 import router from './router'
 
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import { api } from './common/api.js'  // API 链接
-import { post, get, put, toJson, isEmpty } from './common/http.js' // 请求方式中间件
+import './icons' // icon
+import './permission' // permission control
+import './utils/error-log' // error log
 
-//定义全局变量
-Vue.prototype.$api = api;
-Vue.prototype.$post = post;
-Vue.prototype.$get = get;
-Vue.prototype.$put = put;
-Vue.prototype.$toJson = toJson;
-Vue.prototype.$isEmpty = isEmpty;
+import * as filters from './filters' // global filters
+
+/**
+ * If you don't want to use mock-server
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
+ *
+ * Currently MockJs will be used in the production environment,
+ * please remove it before going online ! ! !
+ */
+if (process.env.NODE_ENV === 'production') {
+  const { mockXHR } = require('../mock')
+  mockXHR()
+}
+
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium', // set element-ui default size
+  locale: enLang // 如果使用中文，无需设置，请删除
+})
+
+// register global utility filters
+Object.keys(filters).forEach(key => {
+  Vue.filter(key, filters[key])
+})
 
 Vue.config.productionTip = false
-Vue.use(ElementUI);
 
 new Vue({
-  render: h => h(App),
+  el: '#app',
   router,
-}).$mount('#app')
+  store,
+  render: h => h(App)
+})
